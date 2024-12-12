507f22d2c9c0fc3520c7d904092abbe75b130841
nasiibyare <nasiibyare51@gmail.com>
2024-12-12 07:09:26 -0800

    new update

--- exercises 30-40/exercise 35/main.js
+++ exercises 30-40/exercise 35/main.js
@@ -3,8 +3,15 @@ let image = document.querySelector("img");
 
 function changeimage(){
     let urlImage = prompt("enter your url image ");
+    let border = prompt("enter your border color")
+    let borderRadius = prompt("enter your border raduis")
+    let width = prompt("enter your width")
+    let heigth = prompt("enter your heigth")
     image.setAttribute("src",urlImage);
-    image.style.border ="2px solid black";
+    image.style.border =`2px solid ${border}`;
     image.style.padding ="10px";
-    image.style.borderRadius= "10px";
+    image.style.borderRadius= borderRadius;
+    image.style.width = width;
+    image.style.height = heigth;
+    image.style.backgroundColor = "green";
 }
